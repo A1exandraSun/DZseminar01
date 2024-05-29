3a37a4ce53462ef2055c4ab90280a52f49a99a9e
Alexandra <aaleexaandra@email.cz>
2024-05-29 10:20:40 +0200

    Finding the greater and lesser number

---
+++ Zadacha02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу,которая на вход принимает два числа и выдаёт, какое большее, а какое меньшее.
+// a = 5; b = 7 -> max 7
+// a = 2; b = 10 -> max 10
+// a = -9; b = -3 -> max -3
+
+System.Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ввелите число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < numberB)
+{
+    System.Console.WriteLine("Большее число" + numberB);
+}
+
+System.Console.WriteLine("Меньшее число" + numberA);
+
